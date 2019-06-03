ce701e47706665bc9642f61056ebee0a3756abbb
th199 <th199@kgisl.com>
2019-06-03 08:38:19 +0530

    commit3

---
+++ src/dinesh/dog.java
@@ -0,0 +1,18 @@
+package dinesh;
+
+public class dog 
+{
+
+public static void main()
+{
+dog d1=new dog();
+}
+public static void bark() 
+{
+System.out.println("wov");
+}
+public static void play() 
+{
+System.out.println("playing with ball");	
+}
+}
---
+++ src/dinesh/p1.java
@@ -0,0 +1,26 @@
+package dinesh;
+public class p1
+{
+	static int size=27;
+	String name="tommy";
+	static int x=size-5;
+	dog d2=new dog();
+	public static void fun()
+	{
+	if(x<15)
+	{
+	dog.bark();
+	}
+	while(x>3)
+	{
+	dog.play();
+	}
+	}
+public static void main(String args[])
+{
+new p1();
+p1.fun();
+
+}
+}
+
